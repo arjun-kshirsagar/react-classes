5edfce1512b5d4e1537b85fe9f87337789d0e7d9
arjun kshirsagar <kshirsagararjun20@gmail.com>
2024-05-30 22:05:44 +0530

    Getting Started with React

---
+++ src/ProductCard.js
@@ -0,0 +1,9 @@
+function ProductCard(props) {
+    return (
+        <div>
+            <h2>I am ProductCard</h2>
+        </div>
+    );
+}
+
+export default ProductCard;
---
+++ src/Products.js
@@ -0,0 +1,15 @@
+import ProductCard from "./ProductCard";
+function Products() {
+  return (
+    <div>
+      <h1>I am Products</h1>
+      <ProductCard/>
+    </div>
+  );
+}
+
+export default Products;
+
+// html written inside javascript is called JSX
+
+// react is unidirectional and hence its easy to debug and thats why people love react
